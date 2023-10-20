5b761b43f0008730a1123a6cb2de12714cce27a0
UR Raju <riadhasanraju>
2023-10-20 18:17:50 +0600

    server code last push

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express')
-require('dotenv').config()
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 3017
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3017;
 
-
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.dsz3qaf.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,73 +15,93 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-   //  await client.connect();
-   const laptopCollection = client.db('laptopDB').collection('laptops')
-   // CRUD oparation part 
+    //  await client.connect();
+    const laptopCollection = client.db("laptopDB").collection("laptops");
+    const addCollection = client.db("laptopDB").collection("addLaptop");
+    // CRUD oparation part
 
-         app.get('/laptop', async (req,res) => {
-            const allLaptop = await laptopCollection.find().toArray()
-            console.log(allLaptop);
-            res.send(allLaptop)
-         })
-         app.get('/laptop/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = {_id : new ObjectId(id)}
-            const findData = await laptopCollection.findOne(query)
-            console.log(findData);
-            res.send(findData)
-         })
-         app.post('/laptop', async (req, res) => {
-            const postLaptop = req.body;
-            console.log(postLaptop);
-            const result = await laptopCollection.insertOne(postLaptop)
-            console.log(result);
-            res.send(result)
-         })
-         app.put('/laptop/:id', async(req,res) => {
-            const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
-            const option = {upsert : true}
-            const data = req.body
-            const  updateData = {
-               $set : {
-                  name : data.name,
-                   brand : data.brand, 
-                   price : data.price,
-                   select : data.select,
-                   description : data.description,
-                   rating : data.rating,
-                   photo : data.photo
-               }
-            }
-            const result = await laptopCollection.updateOne(filter,updateData, option)
-            res.send(result)
-         })
-         app.delete('/laptop/:id', async (req,res) => {
-            const id = req.params.body;
-            const query = {_id : new ObjectId(id)}
-            const result = await laptopCollection.deleteOne(query)
-            console.log(result);
-            res.send(result)
+    app.get("/addlaptop", async (req, res) => {
+      const getdata = await addCollection.find().toArray();
+      res.send(getdata);
+    });
+    app.post("/addlaptop", async (req, res) => {
+      const postData = req.body;
+      const result = await addCollection.insertOne(postData);
+      res.send(result);
+    });
+    app.delete("/addlaptop/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      // const query = { _id: new ObjectId(id) };
+      const result = await addCollection.deleteOne({ _id: id });
+      console.log(result);
+      res.send(result);
+    });
 
-         })
+    app.get("/laptop", async (req, res) => {
+      const allLaptop = await laptopCollection.find().toArray();
+      console.log(allLaptop);
+      res.send(allLaptop);
+    });
+    app.get("/laptop/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const findData = await laptopCollection.findOne(query);
+      console.log(findData);
+      res.send(findData);
+    });
+    app.post("/laptop", async (req, res) => {
+      const postLaptop = req.body;
+      console.log(postLaptop);
+      const result = await laptopCollection.insertOne(postLaptop);
+      console.log(result);
+      res.send(result);
+    });
+    app.put("/laptop/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
+      const data = req.body;
+      const updateData = {
+        $set: {
+          name: data.name,
+          brand: data.brand,
+          price: data.price,
+          select: data.select,
+          description: data.description,
+          rating: data.rating,
+          photo: data.photo,
+        },
+      };
+      const result = await laptopCollection.updateOne(
+        filter,
+        updateData,
+        option
+      );
+      res.send(result);
+    });
 
+    app.delete("/laptop/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await laptopCollection.deleteOne(query);
+      console.log(result);
+      res.send(result);
+    });
 
- 
-   //  await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    //  await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-  
-    
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => res.send('Server Brows is running'))
-app.listen(port, () => console.log(`Server port is Running ${port}`))
+app.get("/", (req, res) => res.send("Server Brows is running"));
+app.listen(port, () => console.log(`Server port is Running ${port}`));
